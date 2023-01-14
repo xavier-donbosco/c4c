c4023f10df111d62392ee3f44639a6bb01a25287
Xavier Bosco <xavier.bosco@electrolux.com>
2023-01-14 17:59:41 +0530

    adding cpq files

---
+++ models/silver/cpq_custom_fields.py
@@ -0,0 +1,26 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from delta.tables import *
+
+def customfields_df(df):
+    df = df.withColumn("CustomFields", explode_outer(col("CustomFields"))).select("QuoteId" ,"CustomFields.*")
+    return df.groupBy("QuoteId").pivot("Name").agg(first("Content"))
+
+
+def model(dbt, session):
+
+    dbt.config(materialized="table")
+    dbt.config(schema="cpq")
+
+    df = session.read.json("/mnt/cntdlt/bronze/cpq/file_1/")
+
+    df = customfields_df(df)
+    df = df.withColumnRenamed("Customer's Language", "Customer Language")
+    lis = df.columns
+    lis1 = []
+    for i in lis:
+        lis1.append(i.replace(" ", "_" ))
+    for i in range(len(lis)):
+        df = df.withColumnRenamed(lis[i], lis1[i])
+
+    return df
